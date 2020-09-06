72be0f34b9b9766e4c81fedacff5ef4c562e5dda
meach <meachkaddour1@gmail.com>
2020-09-06 19:30:58 +0100

    about page

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curcciulum Vitea</title>
+</head>
+<body>
+<div class="container">
+<embed src="img/cv.pdf#toolbar=0" type="application/pdf" width="100%" height="700px">
+</div>
+</body>
+</html>
